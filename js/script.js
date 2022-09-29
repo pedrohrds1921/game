adfc90a37232b9944e6f2210b1fd1134c2d38973
Pedrohrds <Pedrohrds1921@users.noreply.github.com>
2022-09-28 22:19:57 -0300

    msgwrong

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-let randomNumber= Math.round(Math.random()*10)
-let xAttempts=1
 const screenOne= document.querySelector(".screenOne")
 const screenTwo= document.querySelector(".screenTwo")
-let msgfinal= document.querySelector(".screenTwo h2")
+const msgfinal= document.querySelector(".screenTwo h2")
+const msgWrong= document.querySelector(".msgwrong")
+const wrongMsg= document.querySelector(".msgwrong2")
+let randomNumber= Math.round(Math.random()*10)
+let xAttempts=1
+
 
 function handleClick(event){
 const inputNumber= document.querySelector("#inputNumber")
@@ -14,6 +17,19 @@ if(Number(inputNumber.value) === randomNumber){
     document.querySelector(".screenTwo h2")
     msgfinal.innerText=`Acertou em ${xAttempts} tentativas!`
 }
+
+if(Number(inputNumber.value) !==randomNumber){
+if(msgWrong.classList.contains("hide")){
+    wrongMsg.classList.add("hide")          
+    msgWrong.classList.remove("hide")
+}else{
+    wrongMsg.classList.remove("hide")          
+    msgWrong.classList.add("hide")
+}
+
+
+}
+
 xAttempts++
 }
 
